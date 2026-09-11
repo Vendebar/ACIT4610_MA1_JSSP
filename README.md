ca7bc306bc9de23d085468fbefe8f6d9928eacf2
999CC <carlrolllund@hotmail.com>
2026-09-11 14:06:13 +0200

    divying tasks

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Authors:
 
 Alex Fathman
 Carl Christian Roll-Lund
+Anders
+
+## Specifications
+
+- Selection Strategy: Tournament Selection
+- 
+
+Note to self: we only care about the standard deviation of the final generation
---
+++ gant_builder.py
@@ -0,0 +1,7 @@
+# use matplot.savefig after rendering put the figures in the /figures folder, with prefix date_time_filename
+
+
+#def gant_builder(reconstruction, num_jobs, num_machines):
+
+
+# See function: plot_JSSP_Gantt(reconstruction, JSSP_cur.jobs_num, JSSP_cur.machines_num)
